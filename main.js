4298c7c00f89275974cfae1374f3a56549a7bdf4
luaycom <luaygitaris@gmail.com>
2024-08-26 21:11:40 +0700

    membuat login keluar formulir jika dipencet ikon user

--- main.js
+++ main.js
@@ -6,3 +6,19 @@ hamburger.addEventListener("click", () => {
   menuSearch.classList.toggle("show");
   menuList.classList.toggle("show");
 });
+
+const user = document.getElementById("user");
+const form = document.getElementById("form");
+
+user.addEventListener("click", () => {
+  form.classList.toggle("active");
+  // console.log("asdasd");
+});
+
+document.addEventListener("click", (e) => {
+  const insideUser = user.contains(e.target);
+  const insideForm = form.contains(e.target);
+  if (!insideUser && !insideForm){
+    form.classList.remove("active")
+  }
+})
